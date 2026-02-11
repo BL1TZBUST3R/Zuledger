1b957b20ac74432de5234e7b600c69730e50b3c5
BL1TZBUST3R <alaudrey100602@gmail.com>
2026-02-11 14:24:43 +0800

    Debugging through Web

--- ledger-backend~/routes/web.php
+++ ledger-backend~/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\File;
 Route::fallback(function () {
     return File::get(public_path('index.html'));
 });
+Route::get('/log-test', function () {
+    \Illuminate\Support\Facades\Log::error('TEST ERROR: If you see this, logs are fixed!');
+    return 'Check your Render logs now.';
+});
