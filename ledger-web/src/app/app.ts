234b94a9c16d902a3b62d945dd67e4aba368ba66
BL1TZBUST3R <alaudrey100602@gmail.com>
2026-02-25 09:08:53 +0800

    Debugging Problems P3

--- ledger-web/src/app/app.ts
+++ ledger-web/src/app/app.ts
@@ -1,24 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, Router } from '@angular/router'; // Removed RouterLink/Active
-
-// 👇 1. Import the Sidebar so we can use <app-sidebar>
-import { SidebarComponent } from './components/sidebar/sidebar'; 
+import { RouterOutlet, Router, NavigationEnd, Event } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule, 
-    RouterOutlet, 
-    SidebarComponent // 👈 2. Register it here
-  ],
+  imports: [CommonModule, RouterOutlet, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class AppComponent {
   title = 'ledger-web';
 
-  // We only need the router here to check if we are on Login/Register page
-  constructor(public router: Router) {} 
+  constructor(public router: Router, private cdr: ChangeDetectorRef) {
+    this.router.events.pipe(
+      filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.cdr.detectChanges();
+    });
+  }
 }
